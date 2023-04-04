6676198759ad49787489a270a00aa8ab61bb4148
AmrKhn <as9183701@gmail.com>
2023-04-04 22:44:44 +0500

    Template done with errors

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { HamBurger } from './assets/constant'
+
+function App() {
+
+  return (
+    <div className="App">
+      check <HamBurger />
+    </div>
+  )
+}
+
+export default App
---
+++ src/assets/constant.jsx
@@ -0,0 +1,70 @@
+// Side-Bar-Menu
+import { FaInstagramSquare, FaFacebook } from "react-icons/fa";
+import { SiTiktok, SiPlayerfm } from "react-icons/si";
+import { BsTwitter, BsTwitch } from "react-icons/bs";
+import { RiDiscordFill } from "react-icons/ri";
+import { RxHamburgerMenu } from "react-icons/rx";
+export const Instagram = FaInstagramSquare;
+export const TikTok = SiTiktok;
+export const Twitter = BsTwitter;
+export const Twitch = BsTwitch;
+export const Facebook = FaFacebook;
+export const Discord = RiDiscordFill;
+export const Player = SiPlayerfm;
+export const HamBurger = RxHamburgerMenu;
+
+
+export const MenuList = [
+  "NAAT",
+  (Naat = ["OUR NAAT", "INSTINCT", "UNCAGED", "SILK"]),
+  "ARTISTS",
+  "ABOUT",
+  (About = [
+    "ABOUT MONSTERCAT",
+    "DIVERSITY & INCLUSION",
+    "CODE OF ETHICS",
+    "CONTACT US",
+  ]),
+  "NEWS",
+  "EVENTS",
+  (Events = ["MONSTERCAT EVENTS", "UPCOMING EVENTS"]),
+  "PROGRAMMING",
+  (Programming = [
+    "MonstercatTV",
+    "CALL OF THE WILD",
+    "SILK SHOWCASE",
+    "UPCOMMING SHOWS",
+  ]),
+  "GOLD",
+  "PARTNERS",
+  "PRESS",
+];
+
+export const MUSIC = ["OUR MUSIC", "INSTINCT", "UNCAGED", "SILK"];
+export const ABOUT = [
+  "ABOUT MONSTERCAT",
+  "DIVERSITY & INCLUSION",
+  "CODE OF ETHICS",
+  "ENVIRONMENTAL",
+  "CONTACT US",
+];
+export const EVENTS = ["MONSTERCAT EVENTS EXPERIENCE", "UPCOMING EVENTS"];
+export const PROGRAMMING = [
+  "MonstercatTV",
+  "CALL OF THE WILD",
+  "SILK SHOWCASE",
+  "UPCOMING SHOWS",
+];
+
+// Header-image
+export const HeaderImage =
+  "https://cdx.monstercat.com?encoding=webp&url=https%3A%2F%2Fwww.monstercat.com%2Frelease%2FMCLP017%2Fcover%3F2ee434f2-4499-4c30-a937-4aec2b4ac1fc&width=1024https://cdx.monstercat.com?encoding=webp&url=https%3A%2F%2Fwww.monstercat.com%2Frelease%2FMCLP017%2Fcover%3F2ee434f2-4499-4c30-a937-4aec2b4ac1fc&width=1024";
+export const BodyVideo = "https://www.youtube.com/watch?v=STpHs6Ye4NM";
+
+// Body-center
+export { NaatListCenter } from "./NaatListCenter";
+
+import { BsShare } from "react-icons/bs";
+import { HiOutlinePlay } from "react-icons/hi";
+export const Share = BsShare;
+export const Play = HiOutlinePlay;
